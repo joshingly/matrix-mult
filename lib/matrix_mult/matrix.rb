339af8816ac707eddebaec50d2985cb2febf8e22
Joshua Antonishen <joshua@antonishen.com>
2012-03-10 13:22:43 -0500

    multiplication

--- lib/matrix_mult/matrix.rb
+++ lib/matrix_mult/matrix.rb
@@ -8,6 +8,25 @@ module MatrixMult
       @num_columns = @array.first.length
     end
 
+    def *(matrix)
+      output = Array.new(@num_rows) { Array.new(matrix.num_columns, 0) }
+      @array.each_with_index do |row, row_num|
+        row.each_with_index do |value, column_num|
+          result = row.zip(matrix.get_column(column_num))
+          result.map! {|elem| elem.first * elem.last}
+          result = result.inject { |sum, value| sum += value }
+
+          output[row_num][column_num] = result
+        end
+      end
+
+      MatrixMult::Matrix.new(output)
+    end
+
+    def ==(matrix)
+      matrix.array == @array
+    end
+
     def get_column(column_number)
       @array.map { |row| row[column_number] }
     end
--- spec/matrix_mult/matrix_spec.rb
+++ spec/matrix_mult/matrix_spec.rb
@@ -1,31 +1,42 @@
 require_relative "../spec_helper"
 
 describe "Matrix" do
-  let(:matrix_one) { [[1,2,3],
-                      [4,5,6]] }
+  let(:raw_matrix_one) { [[1,2,3],
+                          [4,5,6]] }
 
-  let(:matrix_two) { [[1,2,3],
-                      [1,5,1],
-                      [1,5,1]] }
+  let(:raw_matrix_two) { [[1,2,3],
+                          [1,5,1],
+                          [1,5,1]] }
+
+  let(:matrix_one) { MatrixMult::Matrix.new(raw_matrix_one) }
+  let(:matrix_two) { MatrixMult::Matrix.new(raw_matrix_two) }
 
   describe "#initialize" do
-    let(:matrix) { MatrixMult::Matrix.new(matrix_one) }
 
     it "should set the numbers of rows and columns" do
-      matrix.num_rows.should be(2)
-      matrix.num_columns.should be(3)
+      matrix_one.num_rows.should be(2)
+      matrix_one.num_columns.should be(3)
     end
   end
 
   describe "#*" do
+    let(:raw_answer) { [[6,27,8],
+                        [15,63,23]] }
+
+    let(:answer) { MatrixMult::Matrix.new(raw_answer) }
+    let(:output) { matrix_one * matrix_two }
+
+    it "should give the right answer" do
+      output.should == answer
+    end
+
   end
 
   describe "#get_coumn" do
-    let(:matrix) { MatrixMult::Matrix.new(matrix_one) }
 
     it "should get the column" do
-      matrix.get_column(1).should == [2,5]
-      matrix.get_column(2).should == [3,6]
+      matrix_one.get_column(1).should == [2,5]
+      matrix_one.get_column(2).should == [3,6]
     end
   end
 end
